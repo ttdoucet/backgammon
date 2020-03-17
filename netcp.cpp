20d6944ce1c29f064eba5dbc79f04ba930ce898d
Todd Doucet <todd@lambentresearch.com>
2020-03-17 16:07:02 -0400

    minor

--- netcp.cpp
+++ netcp.cpp
@@ -2,8 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-//    BgNet *neural = BgNet::readFile("net.w");
     BgNet *neural = readFile("net.w");
-//    neural->writeFile("copy.w");
     writeFile(*neural, "copy.w");
 }
