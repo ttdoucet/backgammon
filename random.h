3ffe44f6322b10e6526860d2249ce397d4d7f6f0
Todd Doucet <todd@lambentresearch.com>
2020-04-03 12:24:06 -0400

    Added class for RNG with normal distribution.

--- random.h
+++ random.h
@@ -1,6 +1,8 @@
 /* Written by Todd Doucet.  See file copyright.txt.
  */
 #pragma once
+#include <random>
+#include <chrono>
 #include <cstdint>
 
 int throw_die();
@@ -10,3 +12,24 @@ void set_seed(uint64_t s);
 void randomize_seed();
 
 void setupRNG(uint64_t user_seed);
+
+class RNG_normal
+{
+public:
+    RNG_normal(double mean = 0, double variance = 1)
+        : distribution( mean, sqrt(variance) ),
+          seed( std::chrono::system_clock::now().time_since_epoch().count() ),
+          generator(seed)
+    {
+    }
+
+    float random()
+    {
+        return distribution(generator);
+    }
+
+private:
+    std::normal_distribution<float>  distribution;
+    unsigned seed;
+    std::default_random_engine generator;
+};
