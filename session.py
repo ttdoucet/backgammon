e5c80dc83570c281299b625d59be8f613921be29
Todd Doucet <todd@lambentresearch.com>
2020-04-04 20:00:19 -0400

    Minor change.

--- session.py
+++ session.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os
 
-games = 1000
+games = 1000000
 alpha = 0.02
 lambda_ = 0.85
 
 def doit(fromfile, tofile):
-    cmd  = f'./train --alpha {alpha} --lambda {lambda_} --games {games} -w {fromfile} -o {tofile}'
+    cmd  = f'./train --alpha {alpha} --lambda {lambda_} --games {games} -w {fromfile} -o {tofile} -e 1000'
     print(cmd)
     os.system(cmd)
 
