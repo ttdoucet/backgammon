9190e5b7236498ee38d85f77837fc54a816dd836
Todd Doucet <todd@lambentresearch.com>
2020-04-07 07:16:25 -0400

    minor

--- session.py
+++ session.py
@@ -11,13 +11,19 @@ def doit(fromfile, tofile):
     cmd  = f'./train --alpha {alpha} --lambda {lambda_} --games {games} -w {fromfile} -o {tofile} -e {every}'
     print(cmd)
     r = os.system(cmd)
+
     if r & 0xff:
+        print("aborted\n");
+        sys.exit(1)
+
+    if r:
+        print("error encountered, exiting\n");
         sys.exit(1)
 
 
 os.system("./train --games 0 -w random -o random.w");
 doit('random.w', 'white-1.w')
 
-for i in range(1, 20):
+for i in range(1, 54):
     doit(f'white-{i}.w', f'white-{i+1}.w')
 
