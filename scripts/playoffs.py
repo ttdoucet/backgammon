d3d4bce95e827e26c79f05dfdcdb2541238b3cb7
Todd Doucet <todd@lambentresearch.com>
2020-09-08 19:34:11 -0400

    Script to run a playoff session.

---
+++ scripts/playoffs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys
+import os
+
+opponent = '~/src/backgammon/good.w'
+trials = 5000
+playoff_cmd = '~/src/backgammon/playoff'
+
+output_file = 'playoffs.txt'
+
+def doit(player, opponent):
+    cmd = f'{playoff_cmd} -e 1000 -n {trials} {player} {opponent} 2>>{output_file}'
+    print(cmd)
+    r = os.system(cmd)
+
+    if r & 0xff:
+        print("aborted\n");
+        sys.exit(1)
+
+    if r:
+        print("error encountered, exiting\n");
+        sys.exit(1)
+
+
+start = 1
+stop = 5
+
+for i in range(start, stop+1):
+    player = f'white-{i}.w'
+    doit(player, opponent)
+
