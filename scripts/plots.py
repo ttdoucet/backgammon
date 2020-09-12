e7f7bbb4d2ce8aa64be93bfa979e74c51178e176
Todd Doucet <todd@lambentresearch.com>
2020-09-12 09:48:39 -0400

    Tweak plots.

--- scripts/plots.py
+++ scripts/plots.py
@@ -12,15 +12,24 @@ def smooth(y):
     return yhat
 
 
+import re
+def seqno(name):
+    v = re.findall(r'\d+', name)
+    return int(v[0])
+
+def sequence(names):
+    return [seqno(name) for name in names]
+
 fig, ax = plt.subplots()
 
 def do_file(filename):
     playoffs = pd.read_csv(filename)
     playoffs.loc[:, 'equity-smoothed'] = smooth(playoffs.equity.values)
+    playoffs.loc[:, 'millions'] = sequence(playoffs.white.values)
 
     name = os.path.basename(filename)
-    playoffs.plot(ax=ax, y='equity', grid=True, label=name)
-    playoffs.plot(ax=ax, y='equity-smoothed', grid=True, label=name+' (smoothed)')
+    playoffs.plot(ax=ax, x='millions', y='equity', grid=True, label=name)
+    playoffs.plot(ax=ax, x='millions', y='equity-smoothed', grid=True, label=name+' (smoothed)')
 
 
 for filename in sys.argv[1:]:
