6585c2c9f7badefe8c06561ba983b6dd890cd3db
Todd Doucet <todd@lambentresearch.com>
2020-09-12 10:00:50 -0400

    More minor.

--- scripts/plots.py
+++ scripts/plots.py
@@ -17,17 +17,17 @@ def sequence(names):
 
 fig, ax = plt.subplots()
 
-def do_file(filename):
+def plot_file(filename):
     playoffs = pd.read_csv(filename)
     playoffs.loc[:, 'equity-smoothed'] = smooth(playoffs.equity.values)
     playoffs.loc[:, 'millions'] = sequence(playoffs.white.values)
 
     name = os.path.basename(filename)
     playoffs.plot(ax=ax, x='millions', y='equity', grid=True, label=name)
-    playoffs.plot(ax=ax, x='millions', y='equity-smoothed', grid=True, label=name+' (smoothed)')
+    # playoffs.plot(ax=ax, x='millions', y='equity-smoothed', grid=True, label=name+' (smoothed)')
 
 
 for filename in sys.argv[1:]:
-    do_file(filename)
+    plot_file(filename)
 
 plt.show()
