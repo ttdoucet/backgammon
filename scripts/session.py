95e0aa3caf02a8918be40eb629964a83a88b7a27
Todd Doucet <todd@lambentresearch.com>
2020-10-23 18:00:53 -0400

    Clean up script.

--- scripts/session.py
+++ scripts/session.py
@@ -18,10 +18,7 @@ def schedule(n):
 games = 1000000
 every = 1000
 
-alpha = 0.0
-lambda_ = 1.0
-
-def doit(fromfile, tofile):
+def doit(fromfile, tofile, alpha=0.0, lambda_=1.0):
     cmd  = f'./train --alpha {alpha} --lambda {lambda_} --games {games} -w {fromfile} -o {tofile}  -d -e {every}'
     print(cmd)
     r = os.system(cmd)
@@ -41,5 +38,5 @@ doit('random.w', 'white-1.w')
 for i in range(1, 400):
     alpha, lambda_ = schedule(i)
     print("i:", i, "alpha: ", alpha, "lambda:", lambda_)
-    doit(f'white-{i}.w', f'white-{i+1}.w')
+    doit(f'white-{i}.w', f'white-{i+1}.w', alpha, lambda_)
 
