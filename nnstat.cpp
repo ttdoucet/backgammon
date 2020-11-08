636d0e339204dc3b054808a3368c73c961061302
Todd Doucet <todd@lambentresearch.com>
2020-11-08 09:15:43 -0500

    Made nnstat work with new network types.

--- nnstat.cpp
+++ nnstat.cpp
@@ -3,31 +3,27 @@
 
 using namespace std;
 
-netv3 neural;
-
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++)
     {
         auto filename = argv[i];
-        bool st = neural.readFile(filename);
-        if (st == false)
-            std::cout << "Could not read net " << filename << "\n";
+
+        unique_ptr<BgNet> bgnet = readBgNet(filename);
+
+        // Presently we have to choose the specific flavor
+        // of bgnet to use, and in particular it must
+        // be something with parameters of the form we expect.
+        // This code is a hold-over from simpler times.
+
+        // auto neural = dynamic_cast<netv3*>(bgnet.get());
+        auto neural = dynamic_cast<Fc_ReLU_H60_I3*>(bgnet.get());
+
+        if (neural == nullptr)
+            cout << "Could not read net " << filename << "\n";
 
         cout << filename << "  magnitudes, ";
-        cout << "V: " <<  neural.params.V.magnitude() << ", ";
-        cout << "M: " << neural.params.M.magnitude() << "\n";
-
-/*
-        cout << neural.params.M;
-
-        matrix<30, 156> foo;
-        std::cin >> foo;
-
-        if (foo == neural.params.M)
-            std::cout << "They are equal\n";
-        else
-            std::cout << "They are not equal\n";
-*/
+        cout << "V: " <<  neural->params.V.magnitude() << ", ";
+        cout << "M: " << neural->params.M.magnitude() << "\n";
     }
 }
